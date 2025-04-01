b1f6b97ccd0ba15cedd155d84f45f684a70df9c6
HugeStep <cscsw0724@naver.com>
2025-04-01 19:59:50 +0900

    1주차 과제

---
+++ JeongMinSeok/week1/likelion_week_1/src/main/java/hugestep/likelion_week_1/LikelionWeek1Application.java
@@ -0,0 +1,13 @@
+package hugestep.likelion_week_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionWeek1Application.class, args);
+	}
+
+}
---
+++ JeongMinSeok/week1/likelion_week_1/src/test/java/hugestep/likelion_week_1/LikelionWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hugestep.likelion_week_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
