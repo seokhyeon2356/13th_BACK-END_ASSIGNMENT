02c05f49bc9e6547276ccdd40271b31d052669c3
conconcc <lesj9217@gmail.com>
2025-04-01 22:58:07 +0900

    1주차 과제 제출

---
+++ LeeSeungJun/week1/LikeLionWeek1/src/test/java/com/conconcc/LikeLionWeek1/LikeLionWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conconcc.LikeLionWeek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
