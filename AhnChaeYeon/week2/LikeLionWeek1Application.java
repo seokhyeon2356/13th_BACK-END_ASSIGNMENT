dd72d2ffd60392629f8d743903f545b3188e47b4
ahncy <ahncy0106@gmail.com>
2025-04-02 14:25:52 +0900

    안채연 - 2주차 백엔드 과제 제출

---
+++ AhnChaeYeon/week2/LikeLionWeek1Application.java
@@ -0,0 +1,13 @@
+package acycycy.LikeLionWeek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeLionWeek1Application.class, args);
+	}
+	
+}
