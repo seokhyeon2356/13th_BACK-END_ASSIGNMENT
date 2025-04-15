46536549762d615d0fce3dc511aad0b042c40596
kkt9253 <kkt9253@naver.com>
2025-04-15 23:06:38 +0900

    BE week2 hw

---
+++ kimkyeongtak/week2/LikeLionWeek2/LikeLionWeek2/src/main/java/kkt9253/likelionweek2/dto/RequestTodoDTO.java
@@ -0,0 +1,15 @@
+package kkt9253.likelionweek2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RequestTodoDTO {
+
+    private String content;
+}
